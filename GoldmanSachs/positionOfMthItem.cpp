157c3f2d85bc7e4fce66f87cfe4542fbcc071f5d
Arsh-ak7 <arshkr07@gmail.com>
2022-01-02 12:03:47 +0530

    Create positionOfMthItem.cpp

---
+++ GoldmanSachs/positionOfMthItem.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+public:
+    int findPosition(int n, int m, int k)
+    {
+        // code here
+        if (m + k <= n + 1)
+            return m + k - 1;
+
+        m = m - (n - k + 1);
+        return (m % n == 0) ? n : m % n;
+    }
+};
